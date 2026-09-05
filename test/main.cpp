ecf846be0c6c73cccc67de844d2a56aae11e9170
Ivan <Kornillovivan707@mail.ru>
2026-09-05 21:58:18 +0300

    Исправлен интерфейс пользователя

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 void main() {
 	double a, h, S;
+	std::cout << "Enter the width and height (example: 1 3): ";
 	std::cin >> a >> h;
 	S = 0.5 * a * h;
+	std::cout << "Area: ";
 	std::cout << S;
 }
