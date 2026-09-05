6f9634cf98b4d046c4b67ec09469958291ff0326
Ivan <Kornillovivan707@mail.ru>
2026-09-05 22:49:13 +0300

    Изменена структура проекта

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+
 #include "Triangle.h"
-void main() {
+int main() {
 	Triangle triangle;
-	double a, h;
+	float a, h;
 	std::cout << "Enter the width and height (example: 1 3): ";
 	std::cin >> a >> h;
 	triangle.set_hight(h);
 	triangle.set_side(a);
 	std::cout << "Area: " << triangle.calculate_area();
+	return 0;
 }
